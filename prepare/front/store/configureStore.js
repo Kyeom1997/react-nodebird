cf8d04d429903cf83d6719f3cb3543c69686939d
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-09-02 00:36:31 +0900

    Update configureStore.js

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,9 +1,13 @@
+import { applyMiddleware, createStore, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
+import { createWrapper } from "next-redux-wrapper";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import reducer from "../reducers";
 import rootSaga from "../sagas";
 
-const configureStore = () => {
+const configureStore = (context) => {
+  console.log(context);
   const sagaMiddleware = createSagaMiddleware();
   const middlewares = [sagaMiddleware];
   const enhancer =
@@ -14,3 +18,9 @@ const configureStore = () => {
   store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
+
+const wrapper = createWrapper(configureStore, {
+  debug: process.env.NODE_ENV === "development",
+});
+
+export default wrapper;
