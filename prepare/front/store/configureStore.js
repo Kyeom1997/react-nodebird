9c175c1a69e7bc491b6a5961e2a09b1dfcfdfed3
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-09-01 22:15:01 +0900

    reduxsaga

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,25 +1,16 @@
-import { createWrapper } from "next-redux-wrapper";
-import { applyMiddleware, createStore, compose } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
 
 import reducer from "../reducers";
+import rootSaga from "../sagas";
 
 const configureStore = () => {
-  const middlewares = [];
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
   const enhancer =
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
       : composeWithDevTools(applyMiddleware(...middlewares));
   const store = createStore(reducer, enhancer);
-  store.dispatch({
-    type: "CHANGE_NICKNAME",
-    data: "hang_kem",
-  });
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
-
-const wrapper = createWrapper(configureStore, {
-  debug: process.env.NODE_ENV === "development",
-});
-
-export default wrapper;
