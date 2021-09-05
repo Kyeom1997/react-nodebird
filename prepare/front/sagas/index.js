0a65dc0e288d9b4a21af4f62458a054a41e27e53
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-09-06 03:35:24 +0900

    saga-effect

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -1,1 +1,79 @@
-export default function* rootSaga() {}
+import { all, call, fork, put, take } from "redux-saga/effects";
+import axios from "axios";
+
+function loginAPI(data) {
+  return axios.post("/api/login", data);
+}
+
+function* logIn(action) {
+  try {
+    const result = yield call(loginAPI, action.data);
+    yield put({
+      type: "LOG_IN_SUCCESS",
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: "LOG_IN_FAILURE",
+      data: err.response.data,
+    });
+  }
+}
+
+function logoutAPI() {
+  return axios.post("/api/logout");
+}
+
+function* logOut() {
+  try {
+    const result = yield call(logoutAPI);
+    yield put({
+      type: "LOG_OUT_SUCCESS",
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: "LOG_OUT_FAILURE",
+      data: err.response.data,
+    });
+  }
+}
+
+function addPostAPI(data) {
+  return axios.post("/api/post", data);
+}
+
+function* addPost(action) {
+  try {
+    const result = yield call(addPostAPI, action);
+    yield put({
+      type: "ADD_POST_SUCCESS",
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: "ADD_POST_FAILURE",
+      data: err.response.data,
+    });
+  }
+}
+
+function* watchLogIn() {
+  yield take("LOG_IN_REQUEST", logIn);
+}
+
+function* watchLogOut() {
+  yield take("LOG_OUT_REQUEST", logOut);
+}
+
+function* watchAddPost() {
+  yield take("ADD_POST_REQUEST", addPost);
+}
+
+export default function* rootSaga() {
+  yield all([
+    fork(watchLogIn), //call
+    fork(watchLogOut),
+    fork(watchAddPost),
+  ]);
+}
